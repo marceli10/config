a2c0ce782a635f4e2c2e4a80de0fc2d209547071
marceli10 <kunajkuuu@gmail.com>
2025-11-23 19:40:56 +0100

    Switch to light theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
 require 'remap'
 require 'config.lazy'
 
-vim.cmd.colorscheme 'catppuccin'
+-- vim.cmd.colorscheme 'catppuccin'
+-- vim.opt.background = 'light'
+-- vim.cmd.colorscheme 'tokyonight-day'
+
+-- vim.opt.background = 'light'
+-- vim.cmd.colorscheme 'nightwolf'
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,12 @@
 -- return {
---	{ 'folke/tokyonight.nvim', version = '*',
---	config = function()
---		vim.cmd.colorscheme("tokyonight-night")
---	end
---	}
---}
+--     'folke/tokyonight.nvim',
+--     version = '*',
+--     priority = 1000,
+--     opts = {
+--         style = 'day',
+--     },
+-- }
+
 -- return {
 -- 	"savq/melange-nvim",
 -- 	enabled = false,
@@ -14,11 +16,36 @@
 -- 	end
 -- }
 
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	opts = {
+-- 		flavour = "frappe",
+-- 	},
+-- 	priority = 1000,
+-- }
+--
+-- return {
+--     'ricardoraposo/nightwolf.nvim',
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--         theme = 'light',
+--     },
+-- }
+
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	opts = {
-		flavour = "frappe",
-	},
-	priority = 1000,
+    'nickkadutskyi/jb.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.cmd 'colorscheme jb'
+        vim.o.background = 'light'
+
+        -- Set Cursor background to Black, text inside to White
+        vim.api.nvim_set_hl(0, 'Cursor', { bg = '#000000', fg = '#ffffff' })
+        vim.api.nvim_set_hl(0, 'CursorInsert', { bg = '#000000', fg = '#ffffff' })
+        vim.opt.guicursor = 'n-v-c:block-Cursor,i:ver25-CursorInsert'
+    end,
 }

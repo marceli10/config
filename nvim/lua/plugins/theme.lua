77101ee6ae5d78c983b7b6ecebb0991e0fd2ae6e
marceli10 <kunajkuuu@gmail.com>
2026-02-05 21:57:45 +0100

    Change theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,34 +1,11 @@
--- return {
---     'nickkadutskyi/jb.nvim',
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         vim.o.termguicolors = true
---         vim.o.background = 'light'
---         vim.cmd 'colorscheme jb'
---
---         vim.opt.guicursor = 'n-v-c-sm:block-Cursor,i:ver25-Cursor,r-cr-o:hor20-Cursor'
---         vim.api.nvim_set_hl(0, 'Cursor', { bg = '#000000'})
---     end,
--- }
-
--- return {
---     'folke/tokyonight.nvim',
---     lazy = false,
---     priority = 1000,
---     opts = {
---         style = 'day',
---     },
--- }
-
--- return {
---     "yorik1984/newpaper.nvim",
---     priority = 1000,
---     config = true,
---     opts={
---         style="light"
---     }
--- }
-
-return { 'projekt0n/github-nvim-theme', name = 'github-theme' }
+return {
+    'nickkadutskyi/jb.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.o.termguicolors = true
+        vim.o.background = 'light'
+        vim.cmd 'colorscheme jb'
+    end,
+}
